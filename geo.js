758917c69353a4863e11ba1110cd3140c07c5767
Alelixir <115121948+Alelixir@users.noreply.github.com>
2022-11-27 20:26:17 -0500

    Adding a few things

--- geo.js
+++ geo.js
@@ -1,1 +1,18 @@
 
+const cities = ["Toronto", "New York", "Tokyo", "Jakarta", "Chonqing", "Delhi", "Seoul", "São Paulo", "Mexico City", "Buenos Aires"];
+const citiesPhotos = [""];
+const quotes = [""]
+
+function randomPhoto(){
+
+    var randomNum = Math.floor(Math.random() * 10);
+    var newPhoto = document.getElementById("img").src = citiesPhotos[randomNum];
+
+}
+
+function randomQuote(){
+
+    var randomNum = Math.floor(Math.random() * 10);
+    var newPhoto = document.getElementById("quote").vaule = quotes[randomNum];
+
+}
