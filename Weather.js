9c27efb7e2b9c855f699dd0c9706e5547fac432d
Molware-12 <115120094+Molware-12@users.noreply.github.com>
2022-12-04 19:29:25 -0500

    Update Weather.js

--- Weather.js
+++ Weather.js
@@ -3,26 +3,26 @@ const findmylocation = () => {
     const success = (position) => 
     {
         //Getting the location from the json and assigning it to variables
-        console.log(position)
+        console.log(position);
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
         //Connecting to openweathermap API and adding in collected lat and lon variables to get current weather details
-        const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=23a8e0c5f4dfb16199caa42184141848&units=metric'
+        const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=23a8e0c5f4dfb16199caa42184141848&units=metric';
         //Fetching information
-        fetch(weatherUrl)
-        .then(res => res.json())
+        fetch(weatherUrl);
+        .then(res => res.json());
         .then(data => {
-            console.log(data)
+            console.log(data);
             //Getting needed information from json fetched from openweathermap API and assigning ito to variables
-            var descrip = data['weather']['0']['description']
-            var temperature = data['main']['temp']
-            var feelslike = data['main']['feels_like']
-            var windspeed = data['wind']['speed']
+            var descrip = data['weather']['0']['description'];
+            var temperature = data['main']['temp'];
+            var feelslike = data['main']['feels_like'];
+            var windspeed = data['wind']['speed'];
             //Applying variables into HTML 
-            temp.innerHTML = 'Temperature: '+temperature+ ' C°'
-            description.innerHTML = 'Current conditions: ' +descrip
-            feels.innerHTML = 'It currently feels like: ' +feelslike+ ' C°'
-            wind.innerHTML = 'Wind Speed: ' +windspeed+ ' Km/h'
+            temp.innerHTML = 'Temperature: '+temperature+ ' C°';
+            description.innerHTML = 'Current conditions: ' +descrip;
+            feels.innerHTML = 'It currently feels like: ' +feelslike+ ' C°';
+            wind.innerHTML = 'Wind Speed: ' +windspeed+ ' Km/h';
         });
     }
     //Unable to retrieve weather alert
@@ -30,7 +30,7 @@ const findmylocation = () => {
         status.textContent = 'Unable to retrieve your location';
     }
     navigator.geolocation.getCurrentPosition(success, error)
-
 }
+    document.querySelector('.weather_buttons').addEventListener('click', findmylocation);
 
-document.querySelector('.weather_buttons').addEventListener('click', findmylocation);
+    
